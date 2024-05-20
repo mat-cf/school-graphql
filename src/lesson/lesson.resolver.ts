8dd62ffd347e3333b413e7628f5f88ada56167da
bluz1n <matheusf.ccosta@hotmail.com>
2024-05-20 09:04:54 -0300

    Create Lesson service and Mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,12 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { LessonType } from "./lesson.type";
+import { LessonService } from "./lesson.service";
 
 @Resolver(_of => LessonType)
 export class LessonResolver {
-
+  constructor(
+    private lessonService: LessonService
+  ) {}
   @Query(_returns => LessonType)
   Lesson() {
     return {
@@ -13,4 +16,16 @@ export class LessonResolver {
       endDate: (new Date()).toISOString(),
     };
   }
+
+  @Mutation(_returns => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
+
 }
+
+
