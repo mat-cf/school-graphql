a5b75b49baeb5c47803112aff79da17363a3c6a1
bluz1n <matheusf.ccosta@hotmail.com>
2024-06-11 15:38:01 -0300

    Create and get students from lesson

--- src/lesson/assign-students-to-lesson.input.ts
+++ src/lesson/assign-students-to-lesson.input.ts
@@ -2,14 +2,14 @@ import { Field, ID, InputType } from "@nestjs/graphql";
 import { IsUUID } from "class-validator";
 
 @InputType()
-export class AssignStudentToLessonInput {
+export class AssignStudentsToLessonInput {
 
   @IsUUID()
   @Field(type => ID)
   lessonId: string;
   
   @IsUUID("4", { each: true })
-  @Field(type => ID)
+  @Field(type => [ID])
   studentIds: string[];
 
 }
--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,12 +1,17 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './lesson.input';
-import { AssignStudentToLessonInput } from './assign-students-to-lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { StudentService } from 'src/student/student.service';
 
 @Resolver((_of) => LessonType)
 export class LessonResolver {
-  constructor(private lessonService: LessonService) {}
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService
+  ) {}
 
   @Query((_returns) => LessonType)
   Lesson(@Args('id') id: string) {
@@ -25,12 +30,17 @@ export class LessonResolver {
     return this.lessonService.createLesson(createLessonInput);
   }
 
-  @Mutation((_returns) => LessonType)
-  assignStudentToLesson(
-    @Args('assignStudentToLesson') assignStudentToLessonInput: AssignStudentToLessonInput 
+  @Mutation(_returns => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput') assignStudentsToLessonInput: AssignStudentsToLessonInput,
   ) {
-    const { lessonId, studentIds } = assignStudentToLessonInput;
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
     return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students);
   }
 
 }
